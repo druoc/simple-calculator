a68becd624c1d90bf2220efb2a9c4f91a6bee1d4
Dave <d.ruocco1987@googlemail.com>
2023-09-09 15:53:53 +0100

    Adds main functionality

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please enter a number:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter a second number:");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What would you like to do?");
+Console.WriteLine("[A] - Addition");
+Console.WriteLine("[B] - Subtraction");
+Console.WriteLine("[C] - Multiplication");
+Console.WriteLine("[D] - Division");
+
+string operationInput = Console.ReadLine();
+string operationUpper = operationInput.ToUpper();
+
+if (operationUpper != "A" && operationUpper != "B" && operationUpper != "C"&& operationUpper != "D")
+{
+    Console.WriteLine("Please choose a valid operation");
+    Console.WriteLine("Press any key to exit");
+    Console.ReadKey();
+
+} else 
+{
+    switch (operationUpper)
+    {
+        case "A":
+            Console.WriteLine($"{a} + {b} = {a + b}");
+            break;
+        case "B":
+            Console.WriteLine($"{a} - {b} = {a + b}");
+            break;
+        case "C":
+            Console.WriteLine($"{a} * {b} = {a + b}");
+            break;
+        case "D":
+            if (b == 0) {
+                Console.WriteLine("You cannot divide by 0!");
+            } else {
+                Console.WriteLine($"{a} / {b} = {a + b}");
+            }
+            break;
+    }
+    Console.ReadKey();
+}
+
+
+    
